7b1fdd3e15f3a3bcefa562223ddb7fefba645a7d
pejoyjp <75529294+pejoyjp@users.noreply.github.com>
2022-12-21 21:01:48 +0000

    Delete lol directory

--- lol/1.js
+++
@@ -1,5 +0,0 @@
-const getChampions = async()=>
-    await (await fetch('http://ddragon.leagueoflegends.com/cdn/12.23.1/data/en_US/champion.json')).json()
-
-
-getChampions().then(value => console.log(value))
